ae02ff2628458b9c42b29803b6306a4d69d25af1
Vikram Dabali <Vikramviki09@gmail.com>
2021-11-29 07:46:14 +0530

    updated our menu-item to navigate withRouter Higher Order Component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className= {`${size} menu-item`}>           
+const MenuItem = ({title, imageUrl, size, history, match, linkUrl}) => {
+    console.log(match)
+    return(
+    <div className= {`${size} menu-item`}onClick = {() => history.push(`${match.url}${linkUrl}`)}>           
     <div style = {{
         backgroundImage:`url(${imageUrl})`
     }} className='background-image' />     
@@ -11,6 +14,6 @@ const MenuItem = ({title, imageUrl, size}) => (
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-);
+)};
 
-export default MenuItem;
+export default withRouter(MenuItem);
