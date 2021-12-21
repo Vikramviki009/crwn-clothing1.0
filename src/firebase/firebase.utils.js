03f1cf20a15531b95c2c8b0c80542d7fb1c65f93
Vikram Dabali <Vikramviki09@gmail.com>
2021-12-21 19:06:36 +0530

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
-import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
     apiKey: "AIzaSyAbhr8NWNrvgwNblfgAlfYbgKIWOtkaEzg",
@@ -18,21 +18,33 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+export const createUserProfileDocument = async (userAuth, additionalDetails) => {
+    if(!userAuth) return;
+
+    const userRef = await firestore.doc(`users1.0/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+
+    if(!snapshot.exists){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalDetails
+            })
+        } catch (error) {
+            console.log('error creating user', error.messagingSenderId())
+        }
+    }
+
+    return userRef;
+}
+
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
-
-
-
-
-
-
-
-
-
-
-
-
-
